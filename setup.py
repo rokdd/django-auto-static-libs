36fe55d920cf9990a0abee926101c4e0a604b285
rokdd <rokdd@web.de>
2021-12-26 17:22:20 +0100

    Initial 0.0.1

---
+++ setup.py
@@ -0,0 +1,36 @@
+from os.path import join, dirname
+from distutils.core import setup
+
+try:
+    f = open(join(dirname(__file__), 'README.md'))
+    long_description = f.read()
+    f.close()
+except IOError:
+    long_description = None
+
+setup(
+    name='django-static-jquery',
+    version='0.0.1',
+    url="https://github.com/rokdd/django-static-libs",
+    description='jQuery and other librarys ready to embed into templates',
+    long_description=long_description,
+    author='rokdd',
+    author_email='r0kdd@yahoo.com',
+    license='BSD',
+    keywords='django jquery fontawesome pygal javascript static'.split(),
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    packages=['django_static_libs'],
+    package_data={
+        'django_static_libs': [
+            'static/static_libs/js/*.js',
+            'static/static_libs/js/*.map',
+        ],
+    },
+)
