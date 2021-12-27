81c310e94153361da4e3d07326fdf804de3aa0f5
rokdd <rokdd@web.de>
2021-12-27 12:18:47 +0100

    Update static-libs-download.py

--- django_static_libs/management/commands/static-libs-download.py
+++ django_static_libs/management/commands/static-libs-download.py
@@ -5,42 +5,42 @@ import pathlib
 from django.conf import settings
 
 class Command(BaseCommand):
-    help = 'Download packages'
+	help = 'Download packages'
 
-    default_package={
-        'ext_ignore':[]
-    }
-    default_packages={
-        'jquery':{
-            'github_repo':"jquery/jquery",
-            'ext_ignore' : [".json"],
-            'syntax':'js'
-        }
-    }
+	default_package={
+		'ext_ignore':[]
+	}
+	default_packages={
+		'jquery':{
+			'github_repo':"jquery/jquery",
+			'ext_ignore' : [".json"],
+			'syntax':'js'
+		}
+	}
 
 	#   def add_arguments(self, parser):
 	#       parser.add_argument('total', type=int, help='Indicates the number of users to be created')
 
-    def handle(self, *args, **kwargs):
-        #total = kwargs['total']
+	def handle(self, *args, **kwargs):
+		#total = kwargs['total']
 
-        for k,pkg in self.default_packages.items():
-        	print('Download and update static lib "%s"' % (k))
+		for k,pkg in self.default_packages.items():
+			print('Download and update static lib "%s"' % (k))
 
-            response = requests.get("https://api.github.com/repos/%s/releases/latest" % (pkg['github_repo']))
-            #print(response.json()["tag_name"])
+			response = requests.get("https://api.github.com/repos/%s/releases/latest" % (pkg['github_repo']))
+			#print(response.json()["tag_name"])
 
-            r = requests.get("https://github.com/%s/archive/refs/tags/%s.zip" % (pkg['github_repo'],response.json()["tag_name"]))
-            z = zipfile.ZipFile(io.BytesIO(r.content))
-            top_folder=z.namelist()[0]
+			r = requests.get("https://github.com/%s/archive/refs/tags/%s.zip" % (pkg['github_repo'],response.json()["tag_name"]))
+			z = zipfile.ZipFile(io.BytesIO(r.content))
+			top_folder=z.namelist()[0]
 
-            for zip_info in z.infolist():
-                if not (zip_info.filename.startswith(top_folder+'dist/') and not pathlib.Path(zip_info.filename).suffix in pkg['ext_ignore']):
-                    continue
-                zip_info.filename = os.path.join(settings.STATIC_ROOT,"static_libs","%s/%s/%s"%(pkg['syntax'],str(k),os.path.basename(zip_info.filename)))
+			for zip_info in z.infolist():
+				if not (zip_info.filename.startswith(top_folder+'dist/') and not pathlib.Path(zip_info.filename).suffix in pkg['ext_ignore']):
+					continue
+				zip_info.filename = os.path.join(settings.STATIC_ROOT,"static_libs","%s/%s/%s"%(pkg['syntax'],str(k),os.path.basename(zip_info.filename)))
 
-                z.extract(zip_info,"")
-                print("Extracted "+os.path.basename(zip_info.filename)+' into '+zip_info.filename)
+				z.extract(zip_info,"")
+				print("Extracted "+os.path.basename(zip_info.filename)+' into '+zip_info.filename)
 
 
 
