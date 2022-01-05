097adda0169a0e4a8f1e4113c867a8434d0dca92
rokdd <rokdd@web.de>
2022-01-05 17:06:12 +0100

    Update BaseProvider.py

--- django_auto_static_libs/providers/BaseProvider.py
+++ django_auto_static_libs/providers/BaseProvider.py
@@ -1,18 +1,24 @@
 import requests
 
+
 class BaseProvider:
-    def download(self):
-        pass
+	def download(self):
+		pass
+
 
 class SingleUrlProvider(BaseProvider):
-    def __init__(self, url):
-        self.url = url
+	def __init__(self, urls):
+		self.urls = urls
 
-    def download(self):
-        r = requests.get(self.url,allow_redirects=True)
+	def download(self):
+		files = []
+		for url in self.urls:
+			r = requests.get(url, allow_redirects=True)
 
-        # we got not a valid answer of a file:
-        if not r.ok or r.content is None:
-            return None
-
-        return r
+			# we got not a valid answer of a file:
+			if not r.ok or r.content is None:
+				continue
+			files.append(r)
+		if files == []:
+			return None
+		return r
