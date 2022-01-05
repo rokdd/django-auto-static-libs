f3d88131476fe56f079619ef09f75a29c7e0a4bb
rokdd <rokdd@web.de>
2022-01-05 19:12:08 +0100

    Update static-libs-download.py

--- django_auto_static_libs/management/commands/static-libs-download.py
+++ django_auto_static_libs/management/commands/static-libs-download.py
@@ -65,8 +65,9 @@ class Command(BaseCommand):
 
 					z = zipfile.ZipFile(bytes_zip_buffer)
 					for rfile in r:
-						print(rfile.headers)
-						z.writestr(rfile.headers.get("content-disposition").split("filename=")[1],
+						#print(rfile.headers)
+						#rfile.headers.get("content-disposition").split("filename=")[1]
+						z.writestr(os.path.basename(r.url),
 						           z.io.BytesIO(rfile.content))
 				elif isinstance(r,requests.Response) and r.headers.get('content-type') == "application/zip":
 					z = zipfile.ZipFile(io.BytesIO(r.content))
