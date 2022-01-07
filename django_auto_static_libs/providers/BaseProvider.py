1720cd2a61819b8bb85b742492747749b8fe5dc7
rokdd <rokdd@web.de>
2022-01-07 10:18:37 +0100

    Update BaseProvider.py

--- django_auto_static_libs/providers/BaseProvider.py
+++ django_auto_static_libs/providers/BaseProvider.py
@@ -1,11 +1,11 @@
-import requests
-
+import requests,re
+from django.core.exceptions import ImproperlyConfigured
 
 class BaseProvider:
 	def download(self):
 		pass
 
-
+#download files from multiple urls
 class SingleUrlProvider(BaseProvider):
 	def __init__(self, urls):
 		self.urls = urls
@@ -22,3 +22,26 @@ class SingleUrlProvider(BaseProvider):
 		if files == []:
 			return None
 		return files
+
+#extract a url by the title of a tag of a given webpage
+class SingleUrlByExtractProvider(SingleUrlProvider):
+	def __init__(self, url,words=None,regex=None):
+		self.url = url
+		#when either words or regex set:
+		if words is None and regex is None:
+			raise ImproperlyConfigured("Django-auto-static-libs/"+self.__name__+" needs either words= or regex= argument")
+		if regex is not None:
+			self.regex=regex
+		else:
+			#generate regex
+			self.regex=r'href="(?P<url>.*)".*?>'+"".join([r'(?=.*\%s\b)'%x for x in words])+'.+</a>'
+
+	def download(self):
+		r = requests.get(self.url,allow_redirects=True)
+		match_results = re.search(self.regex, r.text, re.IGNORECASE)
+		url= match_results.group("url")
+		if url is None:
+			return None
+		else:
+			self.urls=[url]
+		return super().download()
