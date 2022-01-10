565c02f2af507ecc7c1b881f3fcc2d97658b0b6c
rokdd <rokdd@web.de>
2022-01-10 13:27:51 +0100

    Update GithubProvider.py

--- django_auto_static_libs/providers/GithubProvider.py
+++ django_auto_static_libs/providers/GithubProvider.py
@@ -3,14 +3,16 @@ import requests, zipfile, io
 import os, re, pathlib, errno
 
 class GithubProvider(BaseProvider):
-    def __init__(self, repo):
+    def __init__(self, repo,file="response_json[tag_name].zip"):
         self.repo = repo
+        self.file=file
     def download(self):
         response = requests.get("https://api.github.com/repos/%s/releases/latest" % (self.repo),
                                 allow_redirects=True)
-
-        r = requests.get("https://github.com/%s/archive/refs/tags/%s.zip" % (
-            self.repo, response.json()["tag_name"]), allow_redirects=True)
+        #to use in format of string
+        response_json=response.json()
+        r = requests.get("https://github.com/%s/archive/refs/tags/%s" % (
+            self.repo, self.file.format(**locals())), allow_redirects=True)
 
         #we got not a valid answer of a file:
         if not r.ok or r.content is None:
